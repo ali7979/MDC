3d552b9decd07d93d5661455fcb6efc37b09b9df
Zoheb Alam <zohebzob@gmail.com>
2025-07-06 23:27:07 +0530

    changed env

--- index.js
+++ index.js
@@ -61,5 +61,5 @@ console.log(user,token)
 
 const PORT =  process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
