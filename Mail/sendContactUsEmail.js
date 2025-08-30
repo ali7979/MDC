5a7583b332b141c95092bad4442a30d5a75b43bb
Zoheb Alam <zohebzob@gmail.com>
2025-08-30 17:23:18 +0530

    Added Update ORder

--- Mail/sendContactUsEmail.js
+++ Mail/sendContactUsEmail.js
@@ -11,19 +11,20 @@ const template = handlebars.compile(source);
 async function sendContactUsEmail(contactData) {
   const htmlToSend = template(contactData);
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.hostinger.com',
-    port: 465,
-    auth: {
-      user: 'support@mamadreamcare.com',
-      pass: 'MDC@babycare$001',
-    },
-  });
+const transporter = nodemailer.createTransport({
+  host: 'smtp.hostinger.com',
+  port: 465,
+//   secure: true, // use TLS if port 465
+  auth: {
+    user: 'support@mamadreamcare.com', // your Hostinger email
+    pass: 'MDC@babycare$001', // your Hostinger email password
+  },
+});
 
   const mailOptions = {
     from: `"Mama Dream Care" <support@mamadreamcare.com>`,
     to: 'support@mamadreamcare.com', // send to your support inbox
-    cc:'aliperwez86@gmail.com',
+    cc:'aliperwez86@gmail.com ,zohebzob@gmail.com',
     subject: `New Contact Us Message from ${contactData.name}`,
     html: htmlToSend,
   };
